32799d2eafd9a6c95275b61557b432571d21f532
ObiJako <Jakob.Oberleitner@students.fh-wels.at>
2022-01-10 11:17:50 +0100

    Vorlage

--- Web_IO/Web_IO/Program.cs
+++ Web_IO/Web_IO/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,29 @@ namespace Web_IO
     {
         static void Main(string[] args)
         {
+        }
+
+        public static void Vorlage()
+        {
+            WebClient client = new WebClient();
+
+            string adress = "https://fhwels.s3.eu-central-1.amazonaws.com/PRO1UE_WS21/testfile.txt";
+            string content = client.DownloadString(adress);
+            Stream contentStream = client.OpenRead(adress);
+            StreamReader reader = new StreamReader(contentStream);
+
+            while (reader.Peek() != -1)
+            {
+                string line = reader.ReadLine();
+                Console.WriteLine(line);
+            }
+
+            string[] lines = content.Split('\n');
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
         }
     }
 }
