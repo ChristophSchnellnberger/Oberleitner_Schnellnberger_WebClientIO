e3bb4aea62d98f784d7140c0414b38794efb6722
Christoph Schnellnberger <s2110438023@fhooe.at>
2022-01-10 12:06:39 +0100

    properties AppData

--- Web_IO/Web_IO/AppData.cs
+++ Web_IO/Web_IO/AppData.cs
@@ -8,9 +8,49 @@ namespace Web_IO
 {
     internal class AppData
     {
+        #region properties
+
+        public string App { get; set; }
+        public string Category { get; set; }
+        public int Rating { get; set; }
+        public int Reviews { get; set; }
+        public string Size { get; set; }
+        public int Installs { get; set; }
+        public Enums.Type Type { get; set; }
+        public double Price { get; set; }
+        public string ContentRating { get; set; }
+        public Enums.Genres Genres { get; set; } 
+        public DateTime LastUpdated { get; set; }
+        public string CurrentVersion { get; set; }
+        public string AndroidVersion { get; set; }
+
+        #endregion
+
+
+
+
+        #region methods
         static void MainAppData()
         {
             //Die Klasse AppData soll jeweils einen Datensatz aus den geladenen Daten repräsentieren (je 1 Zeile) mit den jeweiligen Properties.
+            //App	Category	Rating	Reviews	Size	Installs	Type	Price	Content Rating	Genres	Last Updated	Current Ver	Android Ver
+
+
+
+
+
+
         }
+
+        #endregion
+
+       
+
     }
+
+
+
+
+
+
 }
---
+++ Web_IO/Web_IO/Enums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_IO
+{
+    internal class Enums
+    {
+        #region enums
+        public enum Type
+        {
+            Free,
+            Paid,
+        }
+
+        public enum Genres
+        {
+            Photography,
+            Weather,
+            HealthFitness,
+        }
+        #endregion
+
+    }
+}
